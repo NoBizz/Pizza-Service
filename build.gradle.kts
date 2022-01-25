a2d5aa018abc3c2a47696c9431e4b5bb1ea3c60a
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-25 23:52:17 +0100

    Initial version (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ import org.sourcegrade.submitter.submit
 
 plugins {
     java
+    application
     id("org.sourcegrade.submitter").version("0.4.0")
 }
 
 submit {
-    assignmentId = "h09" // do not change assignmentId
+    assignmentId = "projekt" // do not change assignmentId
     studentId = null // TU-ID  z.B. "ab12cdef"
     firstName = null
     lastName = null
@@ -24,8 +25,13 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
     // JUnit only available in "test" source set (./src/test)
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+application {
+    mainClass.set("projekt.Main")
 }
 
 java {
---
+++ src/main/java/projekt/food/Extra.java
@@ -0,0 +1,27 @@
+package projekt.food;
+
+
+/**
+ * A modification that targets configurable values in a {@link Food.Config}.
+ *
+ * @param <C> The target {@link Food.Config} type
+ */
+public interface Extra<C extends Food.Config> {
+
+    /**
+     * The name of this extra.
+     *
+     * @return The name of this extra
+     */
+    String getName();
+
+    /**
+     * The priority of the extra, lower is calculated first
+     */
+    int getPriority();
+
+    /**
+     * Applies the modifications of this extra to the provided {@link C config}.
+     */
+    void apply(C config);
+}
